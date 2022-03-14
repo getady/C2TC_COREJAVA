3d9addaa7a80be9625c58c5d368ad382f18af3c1
ammararab <ammararab777@gmail.com>
2022-03-14 21:41:24 +0530

    Java Keyword

---
+++ KEYWORDS/src/AssertKeyword.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AssertKeyword
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Enter your marks");
+         
+        Scanner sc = new Scanner(System.in);
+         
+        int marks = sc.nextInt();
+         
+        assert marks > 35 : "FAIL";
+    }
+}
