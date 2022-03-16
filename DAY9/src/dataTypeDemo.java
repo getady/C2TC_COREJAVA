9deccb9bb9a75c01d45496dcc5b476a04c83052d
ammararab <ammararab777@gmail.com>
2022-03-16 14:50:55 +0530

    InputStreamReader, BufferReader and DataTypes

---
+++ DAY9/src/dataTypeDemo.java
@@ -0,0 +1,28 @@
+
+public class dataTypeDemo {
+	public static void main (String args[])
+	{
+		
+		int var1 = 2147483647;  //max int value is 2147483647
+		float var2 = 12.2f;
+		double var3 = 12.1;
+		char  var4 = 'A';
+		boolean var5 = true;
+		byte var6 = 127;
+		short var7 = 32767;
+		long var8 = 500L;
+		
+		System.out.println(var1+"\n");
+		System.out.println(var2+"\n");
+		System.out.println(var3+"\n");
+		System.out.println(var4+"\n");
+		System.out.println(var5+"\n");
+		System.out.println(var6+"\n");
+		System.out.println(var7+"\n");
+		System.out.println(var8+"\n");
+	}
+
+
+}
+
+
