57a0b1758a41007f8bead1ad08c29ef683b92bb4
ammararab <ammararab777@gmail.com>
2022-03-18 00:13:01 +0530

    Interface and Wrapper

---
+++ DAY14/src/wrapper/Demo_wrap.java
@@ -0,0 +1,21 @@
+
+package wrapper;
+
+public class Demo_wrap {
+
+	public static void main(String[] args) {
+		
+		int i=30;
+		Integer a=i;
+		Integer j=new Integer(15);
+		System.out.println(a+" "+i+" "+j);
+		//System.out.println(+a);
+		//System.out.println(+j);	
+		//System.out.println(+i);
+	}
+
+}
+
+
+
+
