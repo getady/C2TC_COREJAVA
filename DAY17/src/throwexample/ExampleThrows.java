6e35a1f38153e147fd22885d845e16de458f7d90
ammararab <ammararab777@gmail.com>
2022-03-27 15:47:01 +0530

    Throw and Throws Keywords

---
+++ DAY17/src/throwexample/ExampleThrows.java
@@ -0,0 +1,31 @@
+
+package throwexample;
+
+import java.io.IOException;
+
+public class ExampleThrows 
+{
+	void m1(int num)throws IOException, ClassNotFoundException
+	{
+		if (num==1)
+		throw new IOException ("IOException Occurred");
+		else
+			throw new ClassNotFoundException("ClassNotFoundException");
+	}
+	public static void main(String[] args) 
+	{
+		try {
+			ExampleThrows et=new ExampleThrows();
+			et.m1(1);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
+
