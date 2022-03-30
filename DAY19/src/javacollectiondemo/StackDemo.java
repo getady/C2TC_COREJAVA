63776b578c7ae5e15aede90a2d32ec9b07645137
ammararab <ammararab777@gmail.com>
2022-03-30 10:25:50 +0530

    collection

---
+++ DAY19/src/javacollectiondemo/StackDemo.java
@@ -0,0 +1,27 @@
+
+package javacollectiondemo;
+
+import java.util.Stack;
+
+public class StackDemo {
+
+	public static void main(String[] args)
+	{
+	  // store 10, 20, 30, 40, 50, 60 in the stack
+		
+		Stack<Integer> obj =new Stack<Integer>();
+		obj.push(10);
+		obj.push(20);
+		obj.push(30);
+		obj.push(40);
+		obj.push(50);
+		
+		System.out.println(obj);
+		
+	    // A stack follows the LIFO (Last In First Out) principle
+		obj.pop();
+		System.out.println(obj);
+	}
+}
+
+
