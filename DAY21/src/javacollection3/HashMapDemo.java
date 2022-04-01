a7b5e4d6724e7d2d693500bba4e49170fa917973
ammararab <ammararab777@gmail.com>
2022-04-01 13:49:55 +0530

    HashMap, HashSet, LinkedHashSet, TreeSet

---
+++ DAY21/src/javacollection3/HashMapDemo.java
@@ -0,0 +1,17 @@
+
+package javacollection3;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		HashMap<Integer,String> hmap=new HashMap<Integer,String>();
+		hmap.put(1, "Kopek");
+		hmap.put(2, "Misaal");
+		System.out.println(hmap);
+
+	}
+
+}
+
---
+++ DAY21/src/javacollection3/TreeSetDemo.java
@@ -0,0 +1,22 @@
+
+package javacollection3;
+import java.util.Set;
+import java.util.TreeSet;
+public class TreeSetDemo 
+{
+		
+		public static void main(String[] args)
+		{
+			Set<Integer> hs=new TreeSet<Integer>();
+			hs.add(11);
+			hs.add(22);
+			hs.add(33);
+			hs.add(44);
+			hs.add(4);
+			System.out.println(hs);
+			hs.add(11);
+			System.out.println(hs);
+		}
+
+}
+
